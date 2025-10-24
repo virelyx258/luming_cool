93c7e822d3895390c4db6b64f3a064805ba5a6b1
Virelyx <hi@virelyx.com>
2025-10-24 23:50:25 +0800

    再次修复评论系统
    再次修复评论系统

--- src/types/config.ts
+++ src/types/config.ts
@@ -73,15 +73,6 @@ export type ProfileConfig = {
 	}[];
 };
 
-export type CommentConfig = {
-    twikoo?: TwikooConfig
-  }
-
-  type TwikooConfig = {
-    envId: string
-    region?: string
-    lang?: string
-  }
 
 export type LicenseConfig = {
 	enable: boolean;
@@ -93,6 +84,16 @@ export type LIGHT_DARK_MODE =
 	| typeof LIGHT_MODE
 	| typeof DARK_MODE
 	| typeof AUTO_MODE;
+
+	export type CommentConfig = {
+    twikoo?: TwikooConfig
+  }
+  
+  type TwikooConfig = {
+    envId: string
+    region?: string
+    lang?: string
+  }
 
 export type BlogPostData = {
 	body: string;
