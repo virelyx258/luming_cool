20c5bb00b2e851632b1c749a7bfcc89ef8b5bd0d
RiseForever <riseforever@riseforever.cn>
2025-11-22 10:37:22 +0800

    变更协议
    变更协议

--- src/config.ts
+++ src/config.ts
@@ -93,8 +93,8 @@ export const profileConfig: ProfileConfig = {
 
 export const licenseConfig: LicenseConfig = {
 	enable: true,
-	name: "CC BY-NC-SA 4.0",
-	url: "https://creativecommons.org/licenses/by-nc-sa/4.0/",
+	name: "CC BY-NC-ND 4.0",
+	url: "https://creativecommons.org/licenses/by-nc-nd/4.0/",
 };
 
 export const expressiveCodeConfig: ExpressiveCodeConfig = {
