01968455562d8ceb64cd2ce21f9d15cb03694954
RiseForever <riseforever@riseforever.cn>
2026-02-24 17:03:00 +0800

    删除 YaHei Consolas 相关引用
    删除 YaHei Consolas 相关引用

--- astro.config.mjs
+++ astro.config.mjs
@@ -86,7 +86,7 @@ export default defineConfig({
                 borderRadius: "0.75rem",
                 borderColor: "none",
                 codeFontSize: "0.875rem",
-                codeFontFamily: "'YaHei Consolas Hybrid', ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, 'Liberation Mono', 'Courier New', monospace",
+                codeFontFamily: "ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, 'Liberation Mono', 'Courier New', monospace",
                 codeLineHeight: "1.5rem",
                 frames: {
                     editorBackground: "var(--codeblock-bg)",
