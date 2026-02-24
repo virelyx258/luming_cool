41c607ea82e130ab11f282da99b21c121ae5c65b
RiseForever <riseforever@riseforever.cn>
2026-02-24 18:35:34 +0800

    恢复 MiSans 相关引用
    恢复 MiSans 相关引用

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["sans-serif", ...defaultTheme.fontFamily.sans],
+        sans: ["MiSans", "sans-serif", ...defaultTheme.fontFamily.sans],
       },
     },
   },
